ab5a814e946dc23fa7c7f67171b19c52f578888a
aurelian <oancea@gmail.com>
2015-12-22 00:01:55 +0100

    Added more style

--- src/bombinate/styles/app.clj
+++ src/bombinate/styles/app.clj
@@ -1,8 +1,15 @@
 (ns bombinate.styles.app
   (:require [garden.def :refer [defstyles]]
-            [garden.units :refer [px]]))
+            [garden.units :refer [px percent]]))
 
 (defstyles screen
+  [:html {:font-size (px 15)}]
   [:body
-   {:font-size (px 16)
-    :line-height 1.5}])
+   {:width (percent 87.5)
+    :color "#111"
+    :margin-left "auto"
+    :margin-right "auto"}]
+  [:h1 {:font-weight 400}]
+  [:h2 {:font-weight 400}]
+  [:h3 {:font-weight 400}])
+
