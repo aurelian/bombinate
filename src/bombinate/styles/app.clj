ed3659523f1c230eb654da71a691035675880618
aurelian <oancea@gmail.com>
2015-12-22 23:48:51 +0100

    styling this

--- src/bombinate/styles/app.clj
+++ src/bombinate/styles/app.clj
@@ -1,15 +1,18 @@
-(ns bombinate.styles.app
+(ns ^:figwheel-no-load bombinate.styles.app
   (:require [garden.def :refer [defstyles]]
-            [garden.units :refer [px percent]]))
+            [garden.units :as units]))
 
 (defstyles screen
-  [:html {:font-size (px 15)}]
+  [:html {:font-size (units/px 15)}]
   [:body
-   {:width (percent 87.5)
+   {:width (units/percent 87.5)
     :color "#111"
+    :background-color "#fffff8"
+    :padding-left (units/px 12.5)
     :margin-left "auto"
     :margin-right "auto"}]
-  [:h1 {:font-weight 400}]
+  [:h1 {:font-weight 400
+        :margin-top (units/rem 4)}]
   [:h2 {:font-weight 400}]
   [:h3 {:font-weight 400}])
 
