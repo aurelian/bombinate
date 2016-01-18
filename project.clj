f1f33ac775338a8f3c55bc3435b53394d7e7a091
aurelian <oancea@gmail.com>
2016-01-18 22:28:37 +0100

    More stuff:
     * Learned to use ring-logger and ring middlewares
     * load index.html via ring handler

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-core "1.4.0"]
+                 [ring-logger "0.7.5"]
+                 [ring/ring-defaults "0.1.5"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.clojure/core.async "0.2.374"]
                  [reagent "0.5.1"]
@@ -19,12 +20,12 @@
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-ring "0.9.7"]
             [lein-garden "0.2.6"]
-            [lein-figwheel "0.5.0-1"]]
+            [lein-figwheel "0.5.0-3"]]
 
   :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
                                   [org.clojure/tools.nrepl "0.2.10"]
                                   [figwheel-sidecar "0.5.0-1"]]
-                   :source-paths ["src", "dev"]
+                   :source-paths ["src/clj", "src/cljs", "dev"]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
 
   :source-paths ["src/clj", "src/cljs"]
@@ -32,7 +33,7 @@
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
                                     "resources/public/css/style.css"]
 
-	:garden {:builds [{;; Optional name of the build:
+  :garden {:builds [{;; Optional name of the build:
                      :id "dev"
                      ;; Source paths where the stylesheet source code is
                      :source-paths ["src/bombinate/styles"]
@@ -45,6 +46,9 @@
                                 :pretty-print? false}}]}
 
   :ring {:handler bombinate.handler/app
+         :port 4898
+         :auto-refresh? true
+         :nrepl {:start? true}
          :uberwar-name "bombinate.war"}
 
   :uberjar-name "bombinate.jar"
@@ -73,10 +77,9 @@
                            :pretty-print true}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
+             :server-port 4898 ;; default
              ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs ["resources/public/css"]}) ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
@@ -85,7 +88,7 @@
              ;; if you want to embed a ring handler into the figwheel http-kit
              ;; server, this is for simple ring servers, if this
              ;; doesn't work for you just run your own server :)
-             :ring-handler bombinate.handler/app
+             ;; :ring-handler bombinate.handler/app}) ;; <-- figwheel can't find this
 
              ;; To be able to open files in your editor from the heads up display
              ;; you will need to put a script on your path.
@@ -101,4 +104,3 @@
 
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
--- src/clj/bombinate/handler.clj
+++ src/clj/bombinate/handler.clj
@@ -1,19 +1,21 @@
 (ns bombinate.handler
   (:require [bidi.ring :refer (make-handler)]
-            [ring.util.response :as res]))
+            [ring.logger :as logger]
+            [ring.util.response :as res]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
-(defn index-handler
-  [request]
-  (res/response "Hello Index"))
+(defn index-handler [request]
+  (res/resource-response "index.html" {:root "public"}))
 
-(defn article-handler
-  [{:keys [route-params]}]
+(defn article-handler [{:keys [route-params]}]
   (res/response (str "You are viewing article: " (:id route-params))))
 
-(def handler
+(def app-handler
   (make-handler ["/"
                  {"index.html" index-handler
                   ["articles/" :id "/article.html"] article-handler}]))
 
 (def app
-  (-> handler))
+  (-> app-handler
+    (logger/wrap-with-logger)
+    (wrap-defaults api-defaults)))
